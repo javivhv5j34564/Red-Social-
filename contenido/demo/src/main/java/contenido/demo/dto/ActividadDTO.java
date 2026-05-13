216fd3fcf87a6104be90676abe704b24c400660a
Alexbull <fernandezbarrosoalejandro@gmail.com>
2026-05-13 10:28:51 +0200

    Microservicio CONTENIDO

---
+++ contenido/demo/src/main/java/contenido/demo/dto/ActividadDTO.java
@@ -0,0 +1,11 @@
+package contenido.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class ActividadDTO {
+
+    private String idUsuario;
+    private String tipo;
+    private String referenciaId;
+}
---
+++ contenido/demo/src/main/java/contenido/demo/dto/ReaccionDTO.java
@@ -0,0 +1,12 @@
+package contenido.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class ReaccionDTO {
+
+    private String idPublicacion;
+    private String idUsuario;
+    private String tipo;
+}
+
